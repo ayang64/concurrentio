7e30948ee28eb6d33b26d94dd4b4521c566e2824
Ayan George <ayan@ayan.net>
2018-12-21 23:37:31 -0500

    Rename main.go -> multiwriter.go

---
+++ multiwriter.go
@@ -0,0 +1,91 @@
+package concurrentio
+
+import (
+	"context"
+	"io"
+)
+
+type Writer struct {
+	c int // level of concurrency
+	w []io.Writer
+}
+
+func (w *Writer) Write(p []byte) (int, error) {
+	errchan := make(chan error)
+
+	concurrency := func() int {
+		if w.c < 0 {
+			return len(p)
+		}
+		return w.c
+	}()
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	workers := func() (chan<- io.Writer, <-chan struct{}) {
+		rc := make(chan io.Writer, concurrency)
+		done := make(chan struct{}, concurrency)
+
+		go func() {
+			for i := 0; i < concurrency; i++ {
+				go func() {
+					defer func() { done <- struct{}{} }()
+					for {
+						select {
+						case w := <-rc:
+							wl, err := w.Write(p)
+							if err != nil {
+								errchan <- err
+							}
+
+							if wl != len(p) {
+								errchan <- io.ErrShortWrite
+							}
+						case <-ctx.Done():
+							return
+						}
+					}
+				}()
+			}
+		}()
+
+		return rc, done
+	}
+
+	work, done := workers()
+
+	go func() {
+		for i := range w.w {
+			work <- w.w[i]
+		}
+		cancel()
+	}()
+
+	completed := 0
+mainloop:
+	for {
+		select {
+		case err := <-errchan:
+			cancel() // cancel pending workers.
+			return 0, err
+		case <-done:
+			completed++
+			if completed == concurrency {
+				break mainloop
+			}
+		}
+	}
+
+	return len(p), nil
+}
+func MultiWriterWithConcurrency(n int, w ...io.Writer) *Writer {
+	rc := MultiWriter(w...)
+	rc.c = n
+	return rc
+}
+
+func MultiWriter(w ...io.Writer) *Writer {
+	writers := make([]io.Writer, len(w), len(w))
+	copy(writers, w)
+	return &Writer{c: -1, w: writers}
+}
